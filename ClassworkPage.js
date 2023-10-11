3af6ae46d693779556760d49c55b1dcff5b60771
PoripireddiJyothirmai <147608966+PoripireddiJyothirmai@users.noreply.github.com>
2023-10-11 16:26:08 +0530

    Add files via upload

---
+++ ClassworkPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ClassworkPage = () => {
+  return (
+    <div>
+      <h2>Classwork Management</h2>
+      {/* Add your classwork management components and logic here */}
+    </div>
+  );
+};
+
+export default ClassworkPage;
